d79f32e08b752af4c0f45f8e3014df19d3aa634d
Desi <kkk>
2024-03-08 10:41:04 +0100

    Project Initialization

---
+++ ReVend/src/main/java/revend/ReVendApplication.java
@@ -0,0 +1,13 @@
+package revend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReVendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReVendApplication.class, args);
+    }
+
+}
---
+++ ReVend/src/test/java/revend/ReVendApplicationTests.java
@@ -0,0 +1,13 @@
+package revend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReVendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
